7496358e3e96abbcdb9d02fb32c74badd2823487
WeRockStar <vr_freestyle@outlook.com>
2016-06-18 19:14:18 +0700

    Welcome MVP pattern

---
+++ app/src/main/java/com/werocksta/dagger2demo/MainApplication.java
@@ -0,0 +1,11 @@
+package com.werocksta.dagger2demo;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/werocksta/dagger2demo/view/MainActivity.java
+++ app/src/main/java/com/werocksta/dagger2demo/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.werocksta.dagger2demo;
+package com.werocksta.dagger2demo.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.werocksta.dagger2demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
