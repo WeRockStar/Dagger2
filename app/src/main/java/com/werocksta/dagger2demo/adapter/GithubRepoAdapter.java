eb0e8056b1c99340d3f1afab26182a242d3ed76e
WeRockStar <vr_freestyle@outlook.com>
2016-06-20 22:37:25 +0700

    Added adapter

--- app/src/main/java/com/werocksta/dagger2demo/adapter/GithubRepoAdapter.java
+++ app/src/main/java/com/werocksta/dagger2demo/adapter/GithubRepoAdapter.java
@@ -1,4 +1,30 @@
 package com.werocksta.dagger2demo.adapter;
 
-public class GithubRepoAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GithubRepoAdapter extends RecyclerView.Adapter<GithubRepoAdapter.RepoViewHolder> {
+
+    @Override
+    public RepoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RepoViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class RepoViewHolder extends RecyclerView.ViewHolder {
+
+        public RepoViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
