b8cd6f29c1ee6473f4590e35335419f62aafa39b
WeRockStar <vr_freestyle@outlook.com>
2016-07-03 14:17:40 +0700

    Added RecyclerView

--- app/src/main/java/com/werocksta/dagger2demo/view/fragment/RepoFragment.java
+++ app/src/main/java/com/werocksta/dagger2demo/view/fragment/RepoFragment.java
@@ -3,16 +3,28 @@ package com.werocksta.dagger2demo.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.werocksta.dagger2demo.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RepoFragment extends Fragment {
 
+    @BindView(R.id.rvList)
+    RecyclerView rvList;
+
+    public static RepoFragment newInstance(String user) {
+        RepoFragment fragment = new RepoFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("user", user);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
 
     public RepoFragment() {
     }
@@ -22,7 +34,12 @@ public class RepoFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_repo, container, false);
         ButterKnife.bind(this, view);
+        setUpView();
         return view;
+    }
+
+    private void setUpView() {
+
     }
 
 }
