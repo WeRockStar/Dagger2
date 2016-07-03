f4ebcee6dda00467d750c582fc008657a78e07ee
WeRockStar <vr_freestyle@outlook.com>
2016-07-03 14:52:38 +0700

    Call service repo

--- app/src/main/java/com/werocksta/dagger2demo/presenter/RepoPresenterImpl.java
+++ app/src/main/java/com/werocksta/dagger2demo/presenter/RepoPresenterImpl.java
@@ -1,6 +1,14 @@
 package com.werocksta.dagger2demo.presenter;
 
 import com.werocksta.dagger2demo.manager.ApiService;
+import com.werocksta.dagger2demo.model.RepoCollection;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
 
 
 public class RepoPresenterImpl implements RepoPresenter {
@@ -8,13 +16,43 @@ public class RepoPresenterImpl implements RepoPresenter {
     RepoPresenter.View view;
     ApiService service;
 
+    CompositeSubscription subscription;
+
     public RepoPresenterImpl(View view, ApiService service) {
         this.view = view;
         this.service = service;
+        this.subscription = new CompositeSubscription();
     }
 
     @Override
     public void getRepo(String user) {
+        view.loading();
 
+        subscription.add(service.getRepo(user)
+                .subscribeOn(Schedulers.io())
+                .onErrorResumeNext(new Func1<Throwable, Observable<? extends RepoCollection>>() {
+                    @Override
+                    public Observable<? extends RepoCollection> call(Throwable throwable) {
+                        return Observable.error(throwable);
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<RepoCollection>() {
+                    @Override
+                    public void onCompleted() {
+                        view.loadComplete();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        view.getRepoError(e.getMessage());
+                        view.loadComplete();
+                    }
+
+                    @Override
+                    public void onNext(RepoCollection repoCollection) {
+                        view.displayRepo(repoCollection);
+                    }
+                }));
     }
 }
