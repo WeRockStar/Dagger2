166757048a4150d842b6a0155f85d1caddbcfdf2
WeRockStar <vr_freestyle@outlook.com>
2016-07-03 18:54:06 +0700

    Create kotlin test

---
+++ app/src/main/java/com/werocksta/dagger2demo/Main2Activity.kt
@@ -0,0 +1,17 @@
+package com.werocksta.dagger2demo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+}
