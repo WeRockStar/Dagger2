fb0f29acd2b3c16a1e183ebdb5feca155c942214
WeRockStar <vr_freestyle@outlook.com>
2016-07-09 22:06:16 +0700

    Updated gradle

--- app/src/main/java/com/werocksta/dagger2demo/Main2Activity.kt
+++ app/src/main/java/com/werocksta/dagger2demo/Main2Activity.kt
@@ -2,13 +2,14 @@ package com.werocksta.dagger2demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Main2Activity : AppCompatActivity() {
-
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
     }
 
     override fun onStop() {
