78f3ec8de8ba8bcff2d70efe5fcf9dc86615766d
WeRockStar <kotchaphan.m@gmail.com>
2017-05-19 22:21:12 +0700

    Convert adapter to Kotlin 🤘

---
+++ app/src/main/java/com/werocksta/dagger2demo/adapter/GithubRepoAdapter.kt
@@ -0,0 +1,40 @@
+package com.werocksta.dagger2demo.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.werocksta.dagger2demo.R
+import com.werocksta.dagger2demo.model.RepoCollection
+import com.werocksta.dagger2demo.widget.CustomRecyclerView
+
+class GithubRepoAdapter : CustomRecyclerView() {
+
+    private var repositories: List<RepoCollection>? = null
+    private var clickRepository: OnClickRepository? = null
+
+    fun setGithubAdapter(repositories: List<RepoCollection>, clickRepository: OnClickRepository) {
+        this.repositories = repositories
+        this.clickRepository = clickRepository
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.repo_item_row, parent, false)
+        return RepoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val repository = repositories!![position]
+        val viewHolder = holder as RepoViewHolder
+        viewHolder.tvName.text = repository.nameRepo
+        viewHolder.tvLanguage.text = repository.language
+
+        viewHolder.cvRepo.setOnClickListener { v -> clickRepository!!.onClickRepoItem(repository) }
+    }
+
+    override fun getItemCount(): Int = repositories?.size ?: 0
+
+    interface OnClickRepository {
+        fun onClickRepoItem(repo: RepoCollection)
+    }
+
+}
