cfc0b00eccb80c489e0b91b60ac14911284c0015
WeRockStar <kotchaphan.m@gmail.com>
2017-05-26 16:54:55 +0700

    Convert main activity to Kotlin

---
+++ app/src/main/java/com/werocksta/dagger2demo/view/activity/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.werocksta.dagger2demo.view.activity
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+
+import com.werocksta.dagger2demo.MainApplication
+import com.werocksta.dagger2demo.R
+import com.werocksta.dagger2demo.di.component.ActivityComponent
+import com.werocksta.dagger2demo.di.component.DaggerActivityComponent
+import com.werocksta.dagger2demo.di.module.ActivityModule
+import com.werocksta.dagger2demo.view.fragment.MainFragment
+import com.werocksta.dagger2demo.view.fragment.RepoFragment
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var component: ActivityComponent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        component = DaggerActivityComponent.builder()
+                .activityModule(ActivityModule(this))
+                .appComponent((application as MainApplication).component)
+                .build()
+
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.contentContainer, MainFragment())
+                .commit()
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.contentContainer, fragment)
+                .addToBackStack(null)
+                .commit()
+    }
+
+    fun onClickRepoList(user: String) {
+        changeFragment(RepoFragment.newInstance(user))
+    }
+}
