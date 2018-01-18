3989a4e40b66b1fc0c6964ec5e98ec60348847d8
WeRockStar <kotchaphan.m@gmail.com>
2018-01-18 23:24:55 +0700

    Update git ignore

--- app/src/main/java/com/werockstar/dagger2demo/model/GithubUserCollection.kt
+++ app/src/main/java/com/werockstar/dagger2demo/model/GithubUserCollection.kt
@@ -3,6 +3,6 @@ package com.werockstar.dagger2demo.model
 
 import com.google.gson.annotations.SerializedName
 
-data class GithubUserCollection constructor(@SerializedName("login") val username: String = "", @SerializedName("id") val id: Int = 0,
+class GithubUserCollection constructor(@SerializedName("login") val username: String = "", @SerializedName("id") val id: Int = 0,
                                             @SerializedName("avatar_url") val imageUrl: String = "",
                                             @SerializedName("repos_url") val repoUrl: String = "")
